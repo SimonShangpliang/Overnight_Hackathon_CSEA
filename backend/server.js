2b7d206f21c27ca75b98183a8d640827aa64c6fe
iamsimon <simonrema123@gmail.com>
2024-06-28 12:59:10 +0530

    das

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const genAI = new GoogleGenerativeAI("AIzaSyD_-BM_UUFlX7Zr3aZ3thWH5YkhjDS2R8w");
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors(
+
+    
+));
 
 
 
